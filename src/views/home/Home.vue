61138d402d0cfab8c1a1b258834913ce29d77b5e
popinan <13917181481@139.com>
2021-12-14 15:29:12 +0800

    first commit

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import {getHomeMultidata} from 'network/home.js'
 export default {
     name: 'Home',
+    components: { NavBar },
+    data() {
+      return{
+        banner: [],
+        recommend: [],
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list
+      })
+    }
 }
 </script>
 
 <style>
-
+.home-nav {
+  background-color: var(--color-tint);
+}
 </style>
